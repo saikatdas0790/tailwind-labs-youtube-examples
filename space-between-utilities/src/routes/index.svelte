584056966234fae106ffb4db75990cb05d044ae1
Saikat Das <saikatdas0790@gmail.com>
2022-04-11 15:50:43 +0530

    space between utilities

--- space-between-utilities/src/routes/index.svelte
+++ space-between-utilities/src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-10 min-h-screen flex items-center bg-indigo-500">
+  <div class="max-w-2xl mx-auto">
+    <div class="mb-6 bg-indigo-400 h-4 w-40 rounded" />
+    <div
+      class="flex flex-col-reverse space-y-reverse items-center space-y-6 lg:space-y-12">
+      <div class="h-20 w-20 bg-white rounded-lg shadow-xl" />
+      <div class="h-20 w-20 bg-yellow-200 rounded-lg shadow-xl" />
+      <div class="h-20 w-20 bg-teal-300 rounded-lg shadow-xl" />
+      <div class="h-20 w-20 bg-red-300 rounded-lg shadow-xl" />
+    </div>
+  </div>
+</div>
