90531b693949e0bcf162b01d530dd4e7efad8cc7
Saikat Das <saikatdas0790@gmail.com>
2022-04-16 22:27:07 +0530

    css grid

---
+++ css-grid/src/routes/index.svelte
@@ -0,0 +1,18 @@
+<div class="min-h-screen flex items-center bg-purple-500">
+  <div class="flex-1 max-w-4xl mx-auto p-10">
+    <ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-teal-300 rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+      <li class="bg-white rounded-lg shadow-xl"><div class="h-24" /></li>
+    </ul>
+  </div>
+</div>
