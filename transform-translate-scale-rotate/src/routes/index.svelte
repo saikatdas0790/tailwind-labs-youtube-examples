43124094182f3fc836e08d3c0b376a71523d36bb
Saikat Das <saikatdas0790@gmail.com>
2022-04-16 23:15:56 +0530

    transform, translate, scale and rotate

---
+++ transform-translate-scale-rotate/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<div class="p-10 min-h-screen flex items-center justify-center bg-pink-500">
+  <div class="h-32 w-32 relative">
+    <div class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl" />
+    <div
+      class="absolute inset-0 transform hover:rotate-90 hover:translate-x-full hover:scale-125 transition duration-300">
+      <div class="h-full w-full bg-white rounded-lg shadow-2xl" />
+    </div>
+  </div>
+</div>
