24acbfdc72f7a68e15cfe03941a83c7603dfa614
Saikat Das <saikatdas0790@gmail.com>
2022-04-16 23:01:30 +0530

    transitions

---
+++ transitions/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<ul class="grid list-disc mx-auto justify-center items-center gap-y-2 text-lg">
+	<li class="hover:text-gray-600"><a href="/transition-property">Transition Property</a></li>
+	<li class="hover:text-gray-600">
+		<a href="/transition-timing-function">Transition Timing Function</a>
+	</li>
+	<li class="hover:text-gray-600"><a href="/transition-duration">Transition Duration</a></li>
+	<li class="hover:text-gray-600"><a href="/transition-delay">Transition Delay</a></li>
+</ul>
